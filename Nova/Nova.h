2ff393378b44bcb96ee3fc997225a50e32cc6313
KSoto <KReneSoto@gmail.com>
2012-11-29 19:20:59 -0800

    Tried To Add LibNova, Failed

---
+++ Nova/Nova.h
@@ -0,0 +1,13 @@
+//
+//  Nova.h
+//  Nova
+//
+//  Created by Lion User on 29/11/2012.
+//  Copyright (c) 2012 Katie Soto. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Nova : NSObject
+
+@end
---
+++ Nova/Nova.m
@@ -0,0 +1,13 @@
+//
+//  Nova.m
+//  Nova
+//
+//  Created by Lion User on 29/11/2012.
+//  Copyright (c) 2012 Katie Soto. All rights reserved.
+//
+
+#import "Nova.h"
+
+@implementation Nova
+
+@end
